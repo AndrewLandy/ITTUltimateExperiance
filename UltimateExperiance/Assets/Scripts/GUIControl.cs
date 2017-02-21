eec473442b8fda9da765a19f0c33d4ed789bb278
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-02-21 12:23:40 +0000

    Added Characters
    Added Bob and Steve Assets to Characters Folder

---
+++ UltimateExperiance/Assets/Scripts/GUIControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGui()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button")) ;
+        print("You clicked the button");
+    }
+}
