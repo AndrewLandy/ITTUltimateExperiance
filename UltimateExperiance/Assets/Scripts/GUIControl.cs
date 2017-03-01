67df4f9c2618f2d2c6d63445210d227580b82df8
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-03-01 18:06:11 +0000

    Added GUI
    Added the Items for GUI:
    
    Study Button
    Social Button
    Shop Button
    Menu Button
    Main Text Area
    Speakers Portrait
    Backing Image - Used to highlight user imput panel from BG
    
    All Aspects are visual only. No code has been mapped to buttons.

--- UltimateExperiance/Assets/Scripts/GUIControl.cs
+++ UltimateExperiance/Assets/Scripts/GUIControl.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class GUIControl : MonoBehaviour
 {
-    // GameObject player = GameObject.Find("Player");
     private Player player;
 
     public void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
     }
+
     public void studyNormal()
     {
         player.StudyPoints += 5;
