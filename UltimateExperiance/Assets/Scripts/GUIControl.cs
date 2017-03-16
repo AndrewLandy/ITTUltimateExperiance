0ceda93e1780c35f032b95b2a5d4fee2a3806956
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-03-16 13:59:48 +0000

    Revert "Added  functionality for Social, Study"
    This reverts commit f1182d51e025f912f3bab252e4419f1b1017de42.

--- UltimateExperiance/Assets/Scripts/GUIControl.cs
+++ UltimateExperiance/Assets/Scripts/GUIControl.cs
@@ -1,32 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
-using UnityEngine.UI;
 
 public class GUIControl : MonoBehaviour
 {
     private Player player;
-    private Text gameText;
 
     public void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
-        gameText = GameObject.Find("txtGameText").GetComponent<Text>();
-        
     }
 
-    public void study()
+    public void studyNormal()
     {
-        player.raiseStudy(5);
+        player.StudyPoints += 5;
     }
 
-    public void social()
-    {
-        player.raiseSoical(5);
-    }
-
-    public void updateText(string v)
-    {
-        gameText.text = v;
-    }
 }
--- UltimateExperiance/Assets/Scripts/Player.cs
+++ UltimateExperiance/Assets/Scripts/Player.cs
@@ -5,11 +5,8 @@ public class Player : MonoBehaviour {
 
     public int studyPoints;
     public int socialPoints;
-
     public float money;
 
-    private GUIControl guiManager;
-    #region Properties
     public int StudyPoints
     {
         get
@@ -49,12 +46,8 @@ public class Player : MonoBehaviour {
         }
     }
 
-    #endregion
-    
     void Start()
     {
-        guiManager = GameObject.Find("GUIManager").GetComponent<GUIControl>();
-
         StudyPoints = 0;
         SocialPoints = 100;
         Money = 50.00f;
@@ -63,23 +56,7 @@ public class Player : MonoBehaviour {
 
     public void raiseStudy(int pointsGained)
     {
-        if (studyPoints < 100)
-        {
-            guiManager.updateText("You study for a while.\n" +
-                    "You feel like you have a greater understanding of your subjects!");
-
-            StudyPoints += pointsGained;
-            lowerSocial(5);
-        }
-
-        else
-        {
-            guiManager.updateText("You study for a while.\n" +
-                    "Unfourtunatly nothing stuck, maybe you should take a break!");
-
-            lowerSocial(5);
-        }
-            
+        StudyPoints += pointsGained;
     }
 
     public void lowerStudy(int pointsLost)
@@ -89,23 +66,12 @@ public class Player : MonoBehaviour {
 
     public void raiseSoical(int pointsGained)
     {
-        if (socialPoints < 100)
-        {
-            guiManager.updateText("You Socalise for a while.\n" +
-                    "You feel relaxed and refreshed. It was a enjoyable few hours!");
-
-            SocialPoints += pointsGained;
-        }
-
-        else
-            guiManager.updateText("You Socalise for a while.\n" +
-                    "You had a good time, but other then that. nothing has changed!");
+        SocialPoints += pointsGained;
     }
 
     public void lowerSocial(int pointsLost)
     {
-        if(socialPoints >0 )
-            SocialPoints -= pointsLost;
+        SocialPoints -= pointsLost;
     }
 
     public void raiseMoney(float moneyGained)
