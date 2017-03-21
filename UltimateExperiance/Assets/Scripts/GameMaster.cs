2b44a67cd0ff1b53ce07b57392737d79a5f7fa88
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-03-21 13:12:54 +0000

    Added a gameMaster to display game text
    Started writing a class which will display game text to player, and wait
    for ther input before proceding with a new line of code.

---
+++ UltimateExperiance/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+    private Text gameText;
+
+	void Start () {
+
+        gameText = GameObject.Find("txtGameText").GetComponent<Text>();
+
+        startCollege();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void startCollege()
+    {
+        gameText.text = "Congratulations on getting into the Institute of Technology, Tralee!\nTomorrow is your first day of college.";
+        yield return new WaitUntil();
+    }
+}
