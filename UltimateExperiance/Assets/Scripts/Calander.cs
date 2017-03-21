f722f9c6bc3664004c95b0a3be17ad1cae498e10
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-03-21 10:05:28 +0000

    Started the in-game repositry
    Added a skeleton code block for the in-game calander system which will
    allow the game torack what day and week it is. this is being used to as
    the backbone of thew game to ensure the plot playes out as intended.
    
    It is built using a switch statment qhich quires the status of an enum
    property for week day.

---
+++ UltimateExperiance/Assets/Scripts/Calander.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Calander : MonoBehaviour {
+
+    private Text gameText;
+
+    private enum dayType { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
+    private dayType currentDay;
+
+	void Start () {
+        currentDay = dayType.SUNDAY;
+        gameText = GameObject.Find("txtGameText").GetComponent<Text>();
+	}
+
+    public void weekOne()
+    {
+        switch(currentDay)
+        {
+            case dayType.SUNDAY:
+                {
+                    "Congradulations on getting into the Institute of Technology, Tralee.\n"
+
+                    break;
+                }
+
+            case dayType.MONDAY:
+                {
+
+                    break;
+                }
+
+            case dayType.TUESDAY:
+                {
+
+                    break;
+                }
+
+            case dayType.WEDNESDAY:
+                {
+
+                    break;
+                }
+
+            case dayType.THURSDAY:
+                {
+
+                    break;
+                }
+
+            case dayType.FRIDAY:
+                {
+
+                    break;
+                }
+
+            case dayType.SATURDAY:
+                {
+
+                    break;
+                }
+        }//End Switch
+
+    }//End weekOne()
+}
