6aab03f110b51a52a99397b381532b94196e6fd7
AndrewLandy <andrew.t.landy@students.ittralee.ie>
2017-03-28 12:34:54 +0100

    Reorganised GameMaster + Added Coroutine to handle text updates
    Removed unnesicary methods in GameMaster.cs
    Added bool delegate method to be used by coroutine to check for screen
    presses -> Not working correctly
    added a Coroutine to handlr text. pauses after each line of text is
    disaplyed, waiting for input.

--- UltimateExperiance/Assets/Scripts/GameMaster.cs
+++ UltimateExperiance/Assets/Scripts/GameMaster.cs
@@ -1,27 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class GameMaster : MonoBehaviour {
 
     private Text gameText;
+    private IEnumerator CO_updateText;
+
+    delegate bool checkPress();
+    checkPress isPressed;
+
 
 	void Start () {
 
         gameText = GameObject.Find("txtGameText").GetComponent<Text>();
+        CO_updateText = updateText();
 
-        startCollege();
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        isPressed = pressCheckComp();
 
-    void startCollege()
-    {
-        gameText.text = "Congratulations on getting into the Institute of Technology, Tralee!\nTomorrow is your first day of college.";
-        yield return new WaitUntil();
+        StartCoroutine(CO_updateText);
+        
+
     }
+	
+    //This method should be called everytime the screen is pressed
+    private IEnumerator updateText()
+    {
+
+        gameText.text = "Congratulations on getting into the Institute of Technology, Tralee!\n" +
+            "Tomorrow is your first day of college.";
+        yield return new WaitUntil(() => isPressed());
+
+        gameText.text = "Lets get you all setteled in.\nFirst things first, you need food. It's time to go to the shop!";
+        yield return new WaitUntil(() => isPressed());
+
+        gameText.text = "We should take this time to buy lots of food, so where not running to the shop every other day.";
+        //yield return new WaitUntil(() => isPressed());
+
+        //
+        // rest of game text
+        //
+
+
+    }
+
+    private bool pressCheckMobile()
+    {
+        if (Input.touchCount == 1)
+        {
+            RaycastHit2D touchRay = Physics2D.Raycast(Input.touches[0].position, transform.forward);
+
+            if (touchRay.collider.gameObject == GameObject.Find("txtGameText"))
+                return true;
+            else
+                return false;
+        }
+
+        else
+            return false;
+    }
+
+    // Checks if the mouse is pressed, temp method for dev purposes
+    private bool pressCheckComp()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D touchRay = Physics2D.Raycast(Input.mousePosition, transform.forward);
+
+            if (touchRay.collider == GameObject.Find("txtGameText").GetComponent<Collider2D>())
+                return true;
+            else
+                return false;
+        }
+
+        else
+            return false;
+    }
+
 }
